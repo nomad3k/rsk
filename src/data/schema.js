e323b4c68c69f03954165c0eebd6325d1ed38de8
Chris Kemp <chris.kemp3000@gmail.com>
2018-09-24 15:16:02 +0100

    removed model references

--- src/data/schema.js
+++ src/data/schema.js
@@ -8,13 +8,6 @@ import {
 } from './graphql/News/schema';
 
 import {
-  schema as DatabaseSchema,
-  resolvers as DatabaseResolvers,
-  mutations as DatabaseMutations,
-  queries as DatabaseQueries,
-} from './graphql/Database/schema';
-
-import {
   schema as IntlSchema,
   resolvers as IntlResolvers,
   queries as IntlQueries,
@@ -33,41 +26,25 @@ const RootQuery = [
   # 3. Automatically [stitch multiple schemas together](https://www.apollographql.com/docs/graphql-tools/schema-stitching.html) into one larger API
   type RootQuery {
     ${NewsQueries}
-    ${DatabaseQueries}
     ${IntlQueries}
   }
 `,
 ];
 
-const Mutation = [
-  `
-  # # React-Starter-Kit Mutating API
-  # ### This GraphQL schema was built with [Apollo GraphQL-Tools](https://github.com/apollographql/graphql-tools)
-  # _Build, mock, and stitch a GraphQL schema using the schema language_
-  #
-  # **[Schema Language Cheet Sheet](https://raw.githubusercontent.com/sogko/graphql-shorthand-notation-cheat-sheet/master/graphql-shorthand-notation-cheat-sheet.png)**
-  #
-  # 1. Use the GraphQL schema language to [generate a schema](https://www.apollographql.com/docs/graphql-tools/generate-schema.html) with full support for resolvers, interfaces, unions, and custom scalars. The schema produced is completely compatible with [GraphQL.js](https://github.com/graphql/graphql-js).
-  # 2. [Mock your GraphQL API](https://www.apollographql.com/docs/graphql-tools/mocking.html) with fine-grained per-type mocking
-  # 3. Automatically [stitch multiple schemas together](https://www.apollographql.com/docs/graphql-tools/schema-stitching.html) into one larger API
-  type Mutation {
-    ${DatabaseMutations}
-  }
-`,
-];
+const Mutation = [];
 
 const SchemaDefinition = [
   `
   schema {
     query: RootQuery
-    mutation: Mutation
+    # mutation: Mutation
   }
 `,
 ];
 
 // Merge all of the resolver objects together
 // Put schema together into one array of schema strings
-const resolvers = merge(NewsResolvers, DatabaseResolvers, IntlResolvers);
+const resolvers = merge(NewsResolvers, IntlResolvers);
 
 const schema = [
   ...SchemaDefinition,
@@ -75,7 +52,6 @@ const schema = [
   ...Mutation,
 
   ...NewsSchema,
-  ...DatabaseSchema,
   ...IntlSchema,
 ];
 
